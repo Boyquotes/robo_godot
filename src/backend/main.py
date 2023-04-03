fc7d0135452ea27e0ee8049a76baf32452560081
Elisa Flemer <elisaflemer@gmail.com>
2023-04-02 22:28:18 -0300

    backend

---
+++ src/backend/main.py
@@ -0,0 +1,74 @@
+# Programa principal que mostra os jogos cadastrados no banco de dados no console, numa rota API e na página inicial do site
+
+from backend.config import db # Conexão com banco de dados
+from backend.models.Position import Position # Modelo de tabela de jogos
+from flask import Flask, render_template, request # Framework web
+
+# Cria o servidor web
+app = Flask(__name__)
+
+@app.route('/') # Rota inicial
+def index():
+    return render_template('index.html')
+
+@app.route('/api/positions') # Rota API
+def api_positions():
+    # Retorna uma lista com os jogos cadastrados no banco de dados
+    return {
+        'positions': [position.return_json() for position in db.session.query(Position).all()]
+    }
+
+@app.route('/api/positions/<int:id>') # Rota API
+def api_position(id):
+    # Retorna um jogo específico cadastrado no banco de dados
+    return db.session.query(Position).filter(Position.id == id).first().return_json()
+
+@app.route('/api/positions/last') # Rota API
+def api_position_last():
+    # Retorna o último jogo cadastrado no banco de dados
+    return db.session.query(Position).order_by(Position.id.desc()).first().return_json()
+
+@app.route('/api/position', methods=['POST']) # Rota API
+def api_position_post():
+    # Cria um novo jogo no banco de dados
+    position = Position(
+        x=request.json['x'],
+        y=request.json['y'],
+        z=request.json['z'],
+        r=request.json['r'],
+        j1=request.json['j1'],
+        j2=request.json['j2'],
+        j3=request.json['j3'],
+        j4=request.json['j4']
+    )
+    db.session.add(position)
+    db.session.commit()
+    return position.return_json()
+
+
+@app.route('/api/position/<int:id>', methods=['PUT']) # Rota API
+def api_position_put(id):
+    # Atualiza um jogo no banco de dados
+    position = db.session.query(Position).filter(Position.id == id).first()
+    position.x = request.json['x']
+    position.y = request.json['y']
+    position.z = request.json['z']
+    position.r = request.json['r']
+    position.j1 = request.json['j1']
+    position.j2 = request.json['j2']
+    position.j3 = request.json['j3']
+    position.j4 = request.json['j4']
+    db.session.commit()
+    return position.return_json()
+
+@app.route('/api/position/<int:id>', methods=['DELETE']) # Rota API
+def api_position_delete(id):
+    # Deleta um jogo no banco de dados
+    position = db.session.query(Position).filter(Position.id == id).first()
+    db.session.delete(position)
+    db.session.commit()
+    return position.return_json()
+
+# Inicia o servidor web
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/backend/models/Position.py
@@ -0,0 +1,37 @@
+from backend.models.Base import Base # Classe base para criar as tabelas
+from sqlalchemy import Column, Integer, Float # Para criar as colunas da tabela
+
+# Classe que representa a tabela de jogos
+class Position(Base):
+
+    # Nome da tabela
+    __tablename__ = 'positions'
+
+    # Colunas da tabela
+    id = Column(Integer, primary_key=True) # Chave primária
+    x = Column(Float)
+    y = Column(Float)
+    z = Column(Float)
+    r = Column(Float)
+    j1 = Column(Float)
+    j2 = Column(Float)
+    j3 = Column(Float)
+    j4 = Column(Float)
+    
+    # Função que retorna uma representação em string do objeto, com preço em formato brasileiro
+    def __repr__(self):
+        return f'Position(id={self.id}, x={self.x}, y={self.y}, z={self.z}, r={self.r}, j1={self.j1}, j2={self.j2}, j3={self.j3}, j4={self.j4})'
+    
+    # Função que retorna um dicionário com os dados do objeto
+    def return_json(self):
+        return {
+            'id': self.id,
+            'x': self.x,
+            'y': self.y,
+            'z': self.z,
+            'r': self.r,
+            'j1': self.j1,
+            'j2': self.j2,
+            'j3': self.j3,
+            'j4': self.j4
+        }
