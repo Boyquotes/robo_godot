d6db8a6867bb81207eb392ed38f59adfba7a3626
Elisa Flemer <elisaflemer@gmail.com>
2023-04-02 23:42:22 -0300

    godot fazendo request

---
+++ src/simulacao/root.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	$HTTPRequest.request('http://127.0.0.1:5000/api/position')
+
+
+func _on_http_request_request_completed(result, response_code, headers, body):
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	var response = json.get_data()
+	print(response)
