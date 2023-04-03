8fd898beccf4678a13c2e063e3c139491f5cb8b5
Elisa Flemer <elisaflemer@gmail.com>
2023-04-03 17:05:55 -0300

    delete unnecessary data

--- src/RobotArmTest-master/RobotArm.gd
+++ src/RobotArmTest-master/RobotArm.gd
@@ -18,16 +18,7 @@ onready var j4_label = get_node("HUD/CoordPanel/j4Title/j4Value")
 onready var easingx = get_node("EasingX")
 onready var easingy = get_node("EasingY")
 onready var easingz = get_node("EasingZ")
-onready var pose_editor = get_node("PoseEditor/Panel/MarginContainer/ItemList")
 
-var is_z_gizmo_under_mouse = false
-var is_z_gizmo_dragged = false
-var is_camera_dragged = false
-var is_y_gizmo_under_mouse = false
-var is_y_gizmo_dragged = false
-var is_x_gizmo_under_mouse = false
-var is_x_gizmo_dragged = false
-var is_ik_enabled = true
 
 var last_pose = {'g':90, 'wa':90, 'wr':90, 'x':100, 'y':100, 'z':100}
 var pose_changed = false
@@ -39,13 +30,11 @@ var z = 100
 
 var timer = false
 var playing = false
-var pose_selected = 0
 
 var mandatory_keys = ["x", "y", "z", "g", "wa", "wr"]
 
 func _ready():
 	ik = get_node(ik_path)
-#	marker.hide()
 	set_process(true)
 	x_target = 50
 	y_target = 50
@@ -57,136 +46,31 @@ func _ready():
 	ik.calcIK(x_target, y_target, z, 90, 90, 90)
 	
 func _process(delta):
-		
-	if is_ik_enabled and timer:
-#		timer = false # delay timer is disabled
-		ik.calcIK(x_target, y_target, z, 90, 90, 90)
-		x_label.text = str(int(x_target))
-		y_label.text = str(int(y_target))
-		z_label.text = str(int(z))
-		r_label.text = str(int(extra_coordinates.r))
-		j1_label.text = str(int(extra_coordinates.j1))
-		j2_label.text = str(int(extra_coordinates.j2))
-		j3_label.text = str(int(extra_coordinates.j3))
-		j4_label.text = str(int(extra_coordinates.j4))
-		
+	ik.calcIK(x_target, y_target, z, 90, 90, 90)
+	x_label.text = str(int(x_target))
+	y_label.text = str(int(y_target))
+	z_label.text = str(int(z))
+	r_label.text = str(int(extra_coordinates.r))
+	j1_label.text = str(int(extra_coordinates.j1))
+	j2_label.text = str(int(extra_coordinates.j2))
+	j3_label.text = str(int(extra_coordinates.j3))
+	j4_label.text = str(int(extra_coordinates.j4))
 		
 	if pose_changed:
 		move_robot()
 		pose_changed = false
 	
-func _unhandled_input(event):
-	
-	# Drag Z Gizmo
-	if event is InputEventMouseButton and event.is_pressed() and is_z_gizmo_under_mouse:
-		is_z_gizmo_dragged = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#		print("z gizmo being dragged")
-	if event is InputEventMouseButton and !event.is_pressed() and is_z_gizmo_dragged:
-		is_z_gizmo_dragged = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		var positionOnScreen = camera.unproject_position(base_gizmo.get_global_transform().origin)
-		get_viewport().warp_mouse(positionOnScreen)
-#		print("z gizmo released")
-	if event is InputEventMouseMotion and is_z_gizmo_dragged:
-#		print(event.relative)
-		skeleton.rotate_object_local(Vector3(0,1,0), -event.relative.x * PI/360)
-		z = z + -event.relative.x
-		z_label.text = str(z)
-	# Hold down middle mouse to orbit camera around robot arm
-	if event is InputEventMouseButton and event.get_button_index() == 3 and event.is_pressed():
-		is_camera_dragged = true
-	if event is InputEventMouseButton and event.get_button_index() == 3 and !event.is_pressed():
-		is_camera_dragged = false
-	if event is InputEventMouseMotion and is_camera_dragged:
-		$FocusPoint/Gimbal.rotate_object_local(Vector3(0,0,1), event.relative.y * PI/360)
-		$FocusPoint.rotate_object_local(Vector3(0,-1,0), event.relative.x * PI/360)
-		
-	# Scroll mouse to change camera distance from robot arm
-	if event is InputEventMouseButton:
-		if event.get_button_index() == BUTTON_WHEEL_UP:
-			camera.translate(Vector3(0,0,-1))
-		if event.get_button_index() == BUTTON_WHEEL_DOWN:
-			camera.translate(Vector3(0,0,1))
-	
-	# Drag Y Gizmo
-	if event is InputEventMouseButton and event.is_pressed() and is_y_gizmo_under_mouse:
-		is_y_gizmo_dragged = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#		print("y gizmo dragged")
-	if event is InputEventMouseButton and !event.is_pressed() and is_y_gizmo_dragged:
-		is_y_gizmo_dragged = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		var positionOnScreen = camera.unproject_position(base_gizmo.get_global_transform().origin)
-		get_viewport().warp_mouse(positionOnScreen)
-#		print("y gizmo released")
-	if event is InputEventMouseMotion and is_y_gizmo_dragged:
-		y_target = y_target - event.relative.y
-		y_label.text = str(y_target)
-	
-	# Drag X Gizmo
-	if event is InputEventMouseButton and event.is_pressed() and is_x_gizmo_under_mouse:
-		is_x_gizmo_dragged = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-#		print("x gizmo dragged")
-	if event is InputEventMouseButton and !event.is_pressed() and is_x_gizmo_dragged:
-		is_x_gizmo_dragged = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		var positionOnScreen = camera.unproject_position(base_gizmo.get_global_transform().origin)
-		get_viewport().warp_mouse(positionOnScreen)
-#		print("x gizmo released")
-	if event is InputEventMouseMotion and is_x_gizmo_dragged:
-		x_target = x_target + event.relative.x
-		x_label.text = str(x_target)
-	
-func _on_BaseGizmo_mouse_entered():
-	is_z_gizmo_under_mouse = true
-	
-func _on_BaseGizmo_mouse_exited():
-	is_z_gizmo_under_mouse = false
 	
 func _on_ArmIK_servo_moved(base, shoulder, elbow, wrist, gripper):
-	if is_ik_enabled:
-		skeleton.set_rotation_degrees(Vector3(0,90-base,0))
-		$"Armature/002-Shoulder2".set_rotation_degrees(Vector3(shoulder-90, 0, 0))
-		$"Armature/002-Shoulder2/Spatial".set_rotation_degrees(Vector3(elbow-90, 0, 0)) 
-		$"Armature/002-Shoulder2/Spatial/003-Arm2/Spatial2".set_rotation_degrees(Vector3(90-wrist, 0, 0))
-
-func _on_HUD_servo_manually_moved(base, shoulder, elbow, wrist, gripper):
-	if !is_ik_enabled:
-		$"Armature/002-Shoulder2".set_rotation_degrees(Vector3(shoulder-90, 0, 0))
-		$"Armature/002-Shoulder2/Spatial".set_rotation_degrees(Vector3(elbow-90, 0, 0)) 
-		$"Armature/002-Shoulder2/Spatial/003-Arm2/Spatial2".set_rotation_degrees(Vector3(90-wrist, 0, 0))
-		skeleton.set_rotation_degrees(Vector3(0,base-90,0))
-		var format_string = "%d,%d,%d,%d,%d"
-#		print(format_string % [base, shoulder, elbow, wrist, gripper])
-
-func _on_HUD_is_ik_enabled(value):
-	is_ik_enabled = value
-
-func _on_YGizmo_mouse_entered():
-	is_y_gizmo_under_mouse = true
-#	print("YGizmo Enter")
-
-func _on_YGizmo_mouse_exited():
-	is_y_gizmo_under_mouse = false
-#	print("YGizmo Exit")
-
-
-func _on_XGizmo_mouse_entered():
-	is_x_gizmo_under_mouse = true
-#	print("XGizmo Enter")
-
-
-func _on_XGizmo_mouse_exited():
-	is_x_gizmo_under_mouse = false
-#	print("XGizmo Exit")
+	skeleton.set_rotation_degrees(Vector3(0,90-base,0))
+	$"Armature/002-Shoulder2".set_rotation_degrees(Vector3(shoulder-90, 0, 0))
+	$"Armature/002-Shoulder2/Spatial".set_rotation_degrees(Vector3(elbow-90, 0, 0)) 
+	$"Armature/002-Shoulder2/Spatial/003-Arm2/Spatial2".set_rotation_degrees(Vector3(90-wrist, 0, 0))
 
 # PLAY
 func move_robot():
 	playing = true
 	var poses = [last_pose]
-	print(Data.poses.pose)
 	for i in poses.size():
 		if poses[i].has_all(mandatory_keys):
 			easingx.interpolate_property(self, 'x_target', x_target, poses[i].x, Data.poses.speed, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
@@ -197,29 +81,14 @@ func move_robot():
 		easingx.start()
 		easingy.start()
 		easingz.start()
-		#pose_editor.select(i)
-#		print("x:",x_target)
-#		print("y:",y_target)
-#		print("z:",z)
-		yield(get_tree().create_timer(Data.poses.speed + Data.poses.pause), "timeout")
-		#pose_editor.unselect(i)
-	playing = false
-	pose_selected = Data.poses.pose.size()-1
-	#pose_editor.select(pose_selected)
-	
 
+		yield(get_tree().create_timer(Data.poses.speed + Data.poses.pause), "timeout")
+
+	playing = false
 
 
 func _on_Timer_timeout():
 	timer = true
-
-func _on_ItemList_item_selected(index):
-	if !playing:
-		x_target = Data.poses.pose[index].x
-		y_target = Data.poses.pose[index].y
-		z = Data.poses.pose[index].z
-		pose_selected = index
-
 
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
